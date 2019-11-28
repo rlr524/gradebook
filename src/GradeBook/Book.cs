44a4858d8f611dde4e4b348e1401b46bee3aed9b
Rob Ranf <rob@emiyaconsulting.com>
2019-11-27 17:58:46 -0800

    first commit

---
+++ src/GradeBook/Book.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GradeBook
+{
+
+    class Book
+    {
+        // a constructor that explicitly initializes grades
+        public Book(string name)
+        {
+            grades = new List<double>();
+            this.name = name;
+        }
+
+        // here AddGrade is an instance member  (and instance method) of the class "Book" and will be available where we use 
+        // that object as in our Program.cs file where we add a "new Book" object to instantiate the "Book" object...note no use of the static keyword
+        public void AddGrade(double grade)
+        {
+            grades.Add(grade);
+        }
+        // these are instance fields that are associated with the object Book so when Book is available, grades and name are also available;
+        // the object Book contains and encapsulates a list of grades and a name...these two pieces of state are private to the object Book
+        public void ShowStatistics()
+        {
+            var result = 0.0;
+            var highGrade = double.MinValue;
+            var lowGrade = double.MaxValue;
+            foreach (var gradeNumber in grades)
+            {
+                highGrade = Math.Max(gradeNumber, highGrade);
+                lowGrade = Math.Min(gradeNumber, lowGrade);
+                result += gradeNumber;
+            };
+            result /= grades.Count;
+            Console.WriteLine($"The average grade is {result:N2}");
+            //N2 instructs to carry the number out two decimals; N3 would be three, etc.
+            Console.WriteLine($"The highest grade is {highGrade}");
+            Console.WriteLine($"The lowest grade is {lowGrade}");
+        }
+        private List<double> grades;
+        private string name;
+    }
+}
---
+++ src/GradeBook/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace GradeBook
+{
+    // can think of a class as consisting of two things, the state (the data it holds, like the grade numbers below) and the
+    // behavior that acts on that state, such as averaging it or getting a count of how many elements of data
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // here we are referencing the Book() constructor method created in the Book() class created in Book.cs...this is a way of abstraction - encapsulating 
+            // the components of the program so one component does one thing, not many...we bundle both the data (state) and
+            // the behaviors (methods) that work on that data in one place...here we are using the Book() contstructor method to create a new book object
+            var book = new Book("Rob's Grade Book");
+            book.AddGrade(88.0);
+            book.AddGrade(79.2);
+            book.AddGrade(92.3);
+            book.AddGrade(84.5);
+            book.AddGrade(97.1);
+            book.AddGrade(64.3);
+            book.ShowStatistics();
+        }
+    }
+}
